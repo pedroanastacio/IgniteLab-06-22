a5cc1d39dd3154a2ea686b91378226819d881573
pedroanastacio <pedro.anastacio013@gmail.com>
2022-07-04 20:17:29 -0300

    Event page is now responsive

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -41,7 +41,7 @@ export function VideoPlayer(props: VideoPlayerProps) {
       </div>
 
       <div className="p-8 max-w-[1100px] mx-auto">
-        <div className="flex items-start gap-16">
+        <div className="flex items-start flex-col md:flex-row gap-10 md:gap-16">
           <div className="flex-1">
             <h1 className="text-xl font-bold">{data.lesson.title}</h1>
             <p className="mt-4 text-gray-200 leading-relaxed">
@@ -68,10 +68,10 @@ export function VideoPlayer(props: VideoPlayerProps) {
             )}
           </div>
 
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col sm:flex-row md:flex-col gap-4 sm:gap-8 w-full md:w-fit">
             <a
               href=""
-              className="p-4 text-sm text-gray-600 bg-yellow-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-yellow-700 transition-colors"
+              className="p-4 text-sm text-gray-600 bg-yellow-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-yellow-700 transition-colors sm:flex-1"
             >
               <DiscordLogo size={24} />
               Comunidade do Discord
@@ -79,7 +79,7 @@ export function VideoPlayer(props: VideoPlayerProps) {
 
             <a
               href=""
-              className="p-4 text-sm border border-green-500 text-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-500 hover:text-gray-900 transition-colors"
+              className="p-4 text-sm border border-green-500 text-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-500 hover:text-gray-900 transition-colors sm:flex-1"
             >
               <Lightning size={24} />
               Acesse o desafio
@@ -87,20 +87,22 @@ export function VideoPlayer(props: VideoPlayerProps) {
           </div>
         </div>
 
-        <div className="gap-8 mt-20 grid grid-cols-2">
+        <div className="gap-8 mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2">
           <a
             href=""
-            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch justify-between gap-6 hover:bg-gray-600 transition-colors"
           >
-            <div className="bg-yellow-700 h-full p-6 flex items-center text-gray-600">
-              <FileArrowDown size={40} />
-            </div>
-            <div className="py-6 leading-relaxed">
-              <strong className="text-xl">Material complementar</strong>
-              <p className="text-sm text-gray-200 mt-2">
-                Acesse o material complementar para acelerar o seu
-                desenvolvimento
-              </p>
+            <div className="flex gap-6">
+              <div className="bg-yellow-700 h-full p-6 flex items-center text-gray-600">
+                <FileArrowDown size={40} />
+              </div>
+              <div className="py-6 leading-relaxed">
+                <strong className="text-xl">Material complementar</strong>
+                <p className="text-sm text-gray-200 mt-2">
+                  Acesse o material complementar para acelerar o seu
+                  desenvolvimento
+                </p>
+              </div>
             </div>
             <div className="h-full p-6 flex items-center">
               <CaretRight size={24} />
@@ -108,17 +110,19 @@ export function VideoPlayer(props: VideoPlayerProps) {
           </a>
           <a
             href=""
-            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch justify-between gap-6 hover:bg-gray-600 transition-colors"
           >
-            <div className="bg-yellow-700 h-full p-6 flex items-center text-gray-600">
-              <Image size={40} />
-            </div>
-            <div className="py-6 leading-relaxed">
-              <strong className="text-xl">Wallpapers exclusivos</strong>
-              <p className="text-sm text-gray-200 mt-2">
-                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua
-                máquina
-              </p>
+            <div className="flex gap-6">
+              <div className="bg-yellow-700 h-full p-6 flex items-center text-gray-600">
+                <Image size={40} />
+              </div>
+              <div className="py-6 leading-relaxed">
+                <strong className="text-xl">Wallpapers exclusivos</strong>
+                <p className="text-sm text-gray-200 mt-2">
+                  Baixe wallpapers exclusivos do Ignite Lab e personalize a sua
+                  máquina
+                </p>
+              </div>
             </div>
             <div className="h-full p-6 flex items-center">
               <CaretRight size={24} />
